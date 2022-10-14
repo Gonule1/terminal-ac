f5ababa9a4ecb2d775aec5e14aaec3c9be896591
Gönül Elmalı <78858570+Gonule1@users.noreply.github.com>
2022-10-14 09:27:48 +0300

    Add files via upload

---
+++ sayac.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Threading;
+
+namespace sayac 
+{
+    public partial class sayac : Form
+    {
+        public sayac()
+        {
+
+        }
+    }
+}
