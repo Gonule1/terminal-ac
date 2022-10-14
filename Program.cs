fc8d8e529d07182b58b024233a05f2d919e595ae
Gönül Elmalı <78858570+Gonule1@users.noreply.github.com>
2022-10-14 09:24:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gtk;
+
+using System.Windows.Forms;
+using System.Data;
+using Application = Gtk.Application;
+using staj1.Properties;
+using System.Web.UI;
+
+namespace staj1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow kayit = new MainWindow();
+            kayit.Show();
+            Application.Run();
+            
+
+        }
+
+    }
+}
